623bd7c7e42bfb972699c464c76463ebc1075816
Steve Nguyen <Steve.Nguyen@delllabs.net>
2017-04-21 11:34:55 -0500

    hello

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
-print("hello, world")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+driver = webdriver.Chrome("C:\\Users\\steve.nguyen\\Documents\\chromedriver")
+driver.maximize_window()
+driver.get("http://google.com")
+
+
+element = driver.find_element_by_id("lst-ib")
+element.send_keys("books")
+
+element.send_keys(Keys.RETURN)
+time.sleep(1)
+
+try:
+    driver.find_element_by_partial_link_text("Amazon.com: Books").click()
+    print("Pass, you good Steve")
+except NoSuchElementException:
+    print("Failed, no such element Steve")
+
+
+time.sleep(5)
+driver.quit()
